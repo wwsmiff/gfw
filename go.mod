f00b5aa4b784e2054d263ecf7b3bd6d6a842e3ee
wwsmiff <arnav0872@gmail.com>
2026-03-08 10:56:02 +0530

    setup scaffolding

---
+++ go.mod
@@ -0,0 +1,7 @@
+module gfw
+
+go 1.26.1
+
+require github.com/fsnotify/fsnotify v1.9.0
+
+require golang.org/x/sys v0.13.0 // indirect
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"log"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event: ", event)
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error: ", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("./tmp")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-make(chan struct{})
+}
